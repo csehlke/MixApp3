be6f8a8ba339d8503cb69ccfe62dad6323e2d2b2
Carsten Sehlke <carsten.sehlke@tum.de>
2018-10-13 16:39:18 -0400

    send notifications from within the app and from firebase

---
+++ app/src/main/java/edu/gmu/patriothacks/year2018/android/mixapp/Constants.java
@@ -0,0 +1,8 @@
+package edu.gmu.patriothacks.year2018.android.mixapp;
+
+public class Constants {
+
+    public static final String CHANNEL_ID = "mix_notification";
+    public static final String CHANNEL_NAME = "Mix App Notification";
+    public static final String CHANNEL_DESCRIPTION = "MixAppNotification";
+}
---
+++ app/src/main/java/edu/gmu/patriothacks/year2018/android/mixapp/MyFirebaseMessagingService.java
@@ -0,0 +1,32 @@
+package edu.gmu.patriothacks.year2018.android.mixapp;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+
+/**
+ * Created by Belal on 12/8/2017.
+ */
+
+//class extending FirebaseMessagingService
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        //if the message contains data payload
+        //It is a map of custom keyvalues
+        //we can read it easily
+        if(remoteMessage.getData().size() > 0){
+            //handle the data message here
+        }
+
+        //getting the title and the body
+        String title = (remoteMessage.getNotification().getTitle() == null) ? "Mix App" : remoteMessage.getNotification().getTitle();
+        String body = remoteMessage.getNotification().getBody();
+
+        //then here we can use the title and body to build a notification
+        MyNotificationManager.getInstance(this).displayNotification(title, body);
+    }
+}
